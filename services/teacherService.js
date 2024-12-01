c22278f909484da3ad0876537547849d3f84b47e
Erick Freitas <85972033+erickelsas@users.noreply.github.com>
2024-12-01 18:11:37 -0300

    fix: remove password before return

--- services/teacherService.js
+++ services/teacherService.js
@@ -1,10 +1,14 @@
-const { Teacher } = require('../models')
+const { Teacher } = require('../models');
 
 exports.createTeacher = async (teacher) => {
-    try{
+    try {
         const newTeacher = await Teacher.create(teacher);
 
-        return newTeacher;
+        // Remover o campo 'password' antes de retornar
+        const teacherWithoutPassword = newTeacher.toJSON();
+        delete teacherWithoutPassword.password;
+
+        return teacherWithoutPassword;
     } catch (error) {
         throw new Error('Erro ao criar professor: ' + error.message);
     }
